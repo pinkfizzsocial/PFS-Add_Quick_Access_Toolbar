fb472e03f883983718a1dc33354cb82b930571dd
pinkfizzsocial <website@pinkfizz.social>
2022-08-22 10:14:25 +0100

    Create add-quick-access-toolbar.php

---
+++ add-quick-access-toolbar.php
@@ -0,0 +1,188 @@
+<?php
+/*
+* Plugin Name: PFS-Add Quick Access Toolbar
+* Description: This plugin will add a quick access Toolbar to the Admin Bar
+* Version: 1.0.1
+* Author: Pink Fizz Social
+* Author URI: http://pinkfizz.social
+* License: GPL2
+*/
+
+/**
+ * Add Quick Access Toolbar
+ */
+// First level menu
+add_action('admin_bar_menu', 'add_toolbar_items', 100);
+function add_toolbar_items($admin_bar){
+	$admin_bar->add_menu( array(
+		'id'    => 'my-item',
+		'title' => 'Quick Access', // Your menu title
+		'href'  => '', // URL
+		'meta'  => array(
+		 'target' => '_blank',
+		),
+	));
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'MP Courses', // Your submenu title
+		'href'  => '/wp-admin/edit.php?post_type=mpcs-course', // URL
+		'meta'  => array(
+		),
+	));	
+  // Submenus
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'MP Lessons', // Your submenu title
+		'href'  => '/wp-admin/edit.php?post_type=mpcs-lesson', // URL
+		'meta'  => array(
+		),
+	));
+  // Submenus
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'MP Quizzes', // Your submenu title
+		'href'  => '/wp-admin/edit.php?post_type=mpcs-quiz', // URL
+		'meta'  => array(
+		),
+	));
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'MP Memberships', // Your submenu title
+		'href'  => '/wp-admin/edit.php?post_type=memberpressproduct', // URL
+		'meta'  => array(
+		),
+	));	
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'MP Rules', // Your submenu title
+		'href'  => '/wp-admin/edit.php?post_type=memberpressrule', // URL
+		'meta'  => array(
+		),
+	));	
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'MP Downloads', // Your submenu title
+		'href'  => '/wp-admin/edit.php?post_type=mpdl-file', // URL
+		'meta'  => array(
+		),
+	));	
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'MP Settings', // Your submenu title
+		'href'  => '/wp-admin/admin.php?page=memberpress-options#mepr-license', // URL
+		'meta'  => array(
+		),
+	));	
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'MP Subscriptions', // Your submenu title
+		'href'  => '/wp-admin/admin.php?page=memberpress-subscriptions', // URL
+		'meta'  => array(
+		),
+	));	
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'MP Transactions', // Your submenu title
+		'href'  => '/wp-admin/admin.php?page=memberpress-trans', // URL
+		'meta'  => array(
+		),
+	));	
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'MP Reports', // Your submenu title
+		'href'  => '/wp-admin/admin.php?page=memberpress-reports', // URL
+		'meta'  => array(
+		),
+	));	
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'Woo Orders', // Your submenu title
+		'href'  => '/wp-admin/edit.php?post_type=shop_order', // URL
+		'meta'  => array(
+		),
+	));	
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'Woo Products', // Your submenu title
+		'href'  => '/wp-admin/edit.php?post_type=product', // URL
+		'meta'  => array(
+		),
+	));	
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'Woo Export Orders', // Your submenu title
+		'href'  => '/wp-admin/admin.php?page=wc-order-export#segment=common', // URL
+		'meta'  => array(
+		),
+	));	
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'Woo Subscriptions', // Your submenu title
+		'href'  => '/wp-admin/edit.php?post_type=shop_subscription', // URL
+		'meta'  => array(
+		),
+	));	
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'Woo Automate Workflows', // Your submenu title
+		'href'  => '/wp-admin/edit.php?post_type=aw_workflow', // URL
+		'meta'  => array(
+		),
+	));	
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'Elementor Templates', // Your submenu title
+		'href'  => '/wp-admin/edit.php?post_type=elementor_library&tabs_group=library', // URL
+		'meta'  => array(
+		),
+	));	
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'Elementor Theme Builder', // Your submenu title
+		'href'  => '/wp-admin/admin.php?page=elementor-app&ver=3.6.7#/site-editor/templates', // URL
+		'meta'  => array(
+		),
+	));
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'Elementor Landing Pages', // Your submenu title
+		'href'  => '/wp-admin/edit.php?post_type=e-landing-page', // URL
+		'meta'  => array(
+		),
+	));		
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'Users', // Your submenu title
+		'href'  => '/wp-admin/users.php', // URL
+		'meta'  => array(
+		),
+	));	
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'Edit Functions', // Your submenu title
+		'href'  => '/wp-admin/theme-editor.php?file=functions.php&theme=astra-child-theme', // URL
+		'meta'  => array(
+		),
+	));		
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'Menus', // Your submenu title
+		'href'  => '/wp-admin/nav-menus.php', // URL
+		'meta'  => array(
+		),
+	));	
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'Pages', // Your submenu title
+		'href'  => '/wp-admin/edit.php?post_type=page', // URL
+		'meta'  => array(
+		),
+	));		
+	$admin_bar->add_menu( array(
+		'parent' => 'my-item',
+		'title' => 'Media in S3', // Your submenu title
+		'href'  => '/wp-admin/options-general.php?page=amazon-s3-and-cloudfront', // URL
+		'meta'  => array(
+		),
+	));		
+}
